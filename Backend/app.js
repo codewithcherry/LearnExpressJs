8acd0f5bcb4b9cb7693cd441b1f791689a323290
Prashanth Naroju <prashanth.naro@gmail.com>
2024-07-04 18:57:12 +0530

    code the express routes in app.js and routes folder

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,13 @@
 //this is the root file for thr server to get started
 
 const express = require('express')
+const userRouter=require("./routes/user");
+const adminRouter=require("./routes/admin")
 
 const app=express()
 
-app.get("/",(req,res,next)=>{
-    console.log("first middleware");
-    res.send("<html><body><h1>Welcome to express app</body></html>")
-})
+app.use(adminRouter)
+app.use(userRouter)
 
 
 app.listen(3000)
--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -5,7 +5,13 @@ const router=express.Router();
 
 router.get("/add-product",(req,res,next)=>{
     console.log("first middleware");
-    res.send("<html><body><h1>Welcome to express app</body></html>")
+    res.send("<html><body><form action='/view-product' method='POST'><input type=text name='product-title' placeholder='type here'><button type='submit'>send</button></form></body></html>")
 })
 
+router.post("/view-product",(req,res,next)=>{
+    console.log("this is view product page")
+    console.log(req.body)
+    res.redirect("/")
+    
+})
 module.exports=router
