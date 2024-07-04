faa652bf23e2a2dcd67ba6a2f2617c480cb791ba
Prashanth Naroju <prashanth.naro@gmail.com>
2024-07-04 18:44:39 +0530

    add the code for routes folder admin and user tasks routes

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -1,1 +1,11 @@
 //this file to deal with admin related tasks routes
+
+const express=require('express')
+const router=express.Router();
+
+router.get("/add-product",(req,res,next)=>{
+    console.log("first middleware");
+    res.send("<html><body><h1>Welcome to express app</body></html>")
+})
+
+module.exports=router
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,1 +1,10 @@
 //this code is to develop routes for user tasks
+const express=require('express')
+const router=express.Router();
+
+router.get("/",(req,res,next)=>{
+    console.log("first middleware");
+    res.send("<html><body><h1>Welcome to express app</body></html>")
+})
+
+module.exports=router
