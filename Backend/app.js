3f58a624be17d69a622589ab2bebac3aa23cfa68
Prashanth Naroju <prashanth.naro@gmail.com>
2024-07-04 18:29:31 +0530

    add the first nmiddleware to handle the request

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,13 @@
 //this is the root file for thr server to get started
+
+const express = require('express')
+
+const app=express()
+
+app.get("/",(req,res,next)=>{
+    console.log("first middleware");
+    res.send("<html><body><h1>Welcome to express app</body></html>")
+})
+
+
+app.listen(3000)
