20b518389b968537683669a9f5181c0d22cb8e3c
Prashanth Naroju <prashanth.naro@gmail.com>
2024-07-04 19:01:47 +0530

    add code for the bodyparser for the app.js to parse the data

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,13 @@
 //this is the root file for thr server to get started
 
 const express = require('express')
+const bodyParser=require('body-parser')
 const userRouter=require("./routes/user");
 const adminRouter=require("./routes/admin")
 
 const app=express()
+
+app.use(bodyParser.urlencoded({extended:false}))
 
 app.use(adminRouter)
 app.use(userRouter)
