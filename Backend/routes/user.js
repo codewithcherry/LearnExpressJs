c27dcc5e6f6111af70a44d113cebf074e837801b
Prashanth Naroju <prashanth.naro@gmail.com>
2024-07-05 20:58:47 +0530

    import and use path to set the html file as response on client reuest to express server

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,10 +1,12 @@
 //this code is to develop routes for user tasks
 const express=require('express')
+//path is the core module of nodejs to set the global directory for OS to join the path
+const path=require('path')
 const router=express.Router();
 
 router.get("/",(req,res,next)=>{
     console.log("first middleware");
-    res.send("<html><body><h1>Welcome to express app</body></html>")
+    res.sendFile(path.join(__dirname,"../",'views','shop.html'))
 })
 
 module.exports=router
