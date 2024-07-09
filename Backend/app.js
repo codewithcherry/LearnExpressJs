c632dbb2083f6e8843a52a7cec959ded3b43c224
Prashanth Naroju <prashanth.naro@gmail.com>
2024-07-09 15:49:03 +0530

    set the global variable with pug as template engine and views as the global path to find pug files

--- Backend/app.js
+++ Backend/app.js
@@ -1,23 +1,26 @@
 //this is the root file for thr server to get started
 
-const express = require('express')
-const bodyParser=require('body-parser')
+const express = require('express');
+const bodyParser=require('body-parser');
 const userRouter=require("./routes/user");
-const adminData=require("./routes/admin")
+const adminData=require("./routes/admin");
 const path=require('path');
 
 const app=express()
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.set('view engine','pug');
+app.set('views','views');
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.use("/admin",adminData.route)
-app.use(userRouter)
+app.use("/admin",adminData.route);
+app.use(userRouter);
 
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,"views","errorPage.html"));
 })
 
 
-app.listen(3000)
+app.listen(3000);
